b135348233c7cae92cbb16cac229d1a44cc65b26
sjk5766 <wjdrnr6662@gmail.com>
2023-03-24 21:40:14 +0900

    chore: shema.sql, data.sql 추가

---
+++ src/main/resources/query/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO partner_company VALUES(1, '맘스터치', '강남', '2021-10-01', '김맘스');
---
+++ src/main/resources/query/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE collection_history (
+    id  BIGINT NOT NULL AUTO_INCREMENT,
+    partner_company_id BIGINT NOT NULL,
+    amount INTEGER NOT NULL,
+    box_count INTEGER NOT NULL,
+    thumbnail_count INTEGER NOT NULL,
+    note VARCHAR NOT NULL,
+    collected_at TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE collection_thumbnail (
+    id  BIGINT NOT NULL AUTO_INCREMENT,
+    file_path varchar NOT NULL,
+    extension varchar(16) NOT NULL,
+    history_id BIGINT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE partner_company (
+    id  BIGINT NOT NULL AUTO_INCREMENT,
+    name varchar(32) NOT NULL,
+    location varchar(16) NOT NULL,
+    started_at TIMESTAMP NOT NULL,
+    business_name varchar(32) NOT NULL,
+    PRIMARY KEY (id)
+);
