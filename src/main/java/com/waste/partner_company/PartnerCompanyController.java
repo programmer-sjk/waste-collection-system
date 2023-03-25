9dded62c3cfa7d3ea626c97965df5dc3122da8e2
sjk5766 <wjdrnr6662@gmail.com>
2023-03-25 16:15:40 +0900

    feat: dto validation 추가

--- src/main/java/com/waste/partner_company/PartnerCompanyController.java
+++ src/main/java/com/waste/partner_company/PartnerCompanyController.java
@@ -1,5 +1,6 @@
 package com.waste.partner_company;
 
+import com.waste.common.ResponseMessage;
 import com.waste.partner_company.domain.PartnerCompany;
 import com.waste.partner_company.dto.PartnerCompanyRequest;
 import jakarta.validation.Valid;
@@ -25,7 +26,8 @@ public class PartnerCompanyController {
     }
 
     @PostMapping()
-    public void register(@RequestBody PartnerCompanyRequest request) {
+    public ResponseMessage<String> register(@Valid @RequestBody PartnerCompanyRequest request) {
         partnerCompanyService.register(request);
+        return ResponseMessage.ok();
     }
 }
--- src/main/java/com/waste/partner_company/dto/PartnerCompanyRequest.java
+++ src/main/java/com/waste/partner_company/dto/PartnerCompanyRequest.java
@@ -1,13 +1,23 @@
 package com.waste.partner_company.dto;
 
 import com.waste.partner_company.domain.PartnerCompany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public class PartnerCompanyRequest {
+    @NotBlank
+    @Size(max = 32)
     private String name;
+    @NotBlank
+    @Size(max = 16)
     private String location;
+    @NotNull
     private LocalDate startedAt;
+    @NotBlank
+    @Size(max = 32)
     private String businessName;
 
     private PartnerCompanyRequest() {}
