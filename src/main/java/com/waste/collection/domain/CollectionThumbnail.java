0872d234de747135d8e2dedc347f514796d47215
sjk5766 <wjdrnr6662@gmail.com>
2023-03-26 22:30:45 +0900

    refactor: CollectionThumbnail id 전략을 IDENTITY로 수정

--- src/main/java/com/waste/collection/domain/CollectionThumbnail.java
+++ src/main/java/com/waste/collection/domain/CollectionThumbnail.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Entity
 public class CollectionThumbnail extends BaseEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String filePath;
